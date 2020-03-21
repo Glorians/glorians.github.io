af3f6648bd087174a48f97c63d8d165f03fb1a48
Alex <sallexx@outlook.com>
2020-03-21 02:05:12 +0200

    add select product

--- js/main.js
+++ js/main.js
@@ -24,11 +24,39 @@ $(function () {
 
 
 // Mobile Version
-
 if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
     console.log('this is a touch device');
 } else {
     console.log('this is not a touch device');
     document.body.classList.add('no-touch');
 }
+
+
+$(document).ready(function () {
+   $("#btn-close-popup").click(function () {
+       $(".popup-container").css("display", "none")
+   });
+
+    $(".item-product").click(function () {
+
+      let img = $(this).find(".item-product-img");
+      let imgClass = $(img).attr("class");
+      let arrayImgClass = imgClass.split(" ");
+      let background = $("."+arrayImgClass[1]).css("background");
+      let arrayBackground = background.split(" ");
+      let arrayUrl = arrayBackground[4].split('"');
+      let url = arrayUrl[1];
+
+
+      $(".popup-img").attr("src", url);
+
+      $("#btn-close-popup").css("display", "block");
+      $(".popup-container").css("display", "flex");
+    });
+
+});
+
+
+
+
 
